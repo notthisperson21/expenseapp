26a889683d25c4fff0bbbb3193c57d09fcbc6dc4
notthisperson21 <greggreenberger@gmail.com>
2026-04-13 16:02:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get('/')
+def root():
+    return {'message': 'Expense app backend running'}
+
 @app.get('/status')
 def status():
-    return {'status':'ok'}
+    return {'status': 'ok'}
